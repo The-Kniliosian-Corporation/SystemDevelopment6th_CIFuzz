0b483bc56cdc1d8f0953ecbc9b9714ef7d106033
Kristawan Jaroenrad <kristawanja@gmail.com>
2026-06-03 14:52:13 +0900

    Implement modulo operation in TestOneInput function

--- fuzz/fuzz_calculator.py
+++ fuzz/fuzz_calculator.py
@@ -36,9 +36,8 @@ def TestOneInput(data):
          calc.multiply(val_a, val_b)
       elif op == 3:
          calc.divide(val_a, val_b)
-      # TODO: Implement modulo operation
-      # elif op == 4:
-      #    calc.modulo(val_a, val_b)
+      elif op == 4:
+         calc.modulo(val_a, val_b)
 
             
    except (ValueError, InvalidInputException):
